514367727fee5d1102b6d8aa0fbefcf5b25e2905
fanuel nunes <fanuelson@gmail.com>
2016-07-29 01:55:06 -0300

    commit inicial

---
+++ src/test/java/DataUtilsColisaoDataPeriodoTest.java
@@ -0,0 +1,105 @@
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Test;
+
+import ffnunes.utils.DataUtils;
+
+public class DataUtilsColisaoDataPeriodoTest {
+	
+	@Test
+	public void dataIgualAoInicio(){
+		Date inicioPeriodo = new DateTime(2016, 10, 20, 5, 10, 0).toDate();
+		Date data =          new DateTime(2016, 10, 20, 5, 10, 0).toDate();
+		Date fimPeriodo =    new DateTime(2016, 12, 20, 5, 10, 0).toDate();
+		
+		//Act
+		boolean resultado1 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, true, true);
+		boolean resultado2 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, false, true);
+		boolean resultado3 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, true, false);
+		boolean resultado4 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, false, false);
+		
+		//Arrange
+		Assert.assertTrue(resultado1);
+		Assert.assertFalse(resultado2);
+		Assert.assertTrue(resultado3);
+		Assert.assertFalse(resultado4);
+	}
+	
+	@Test
+	public void dataIgualAoFim(){
+		Date inicioPeriodo = new DateTime(2016, 10, 20, 5, 10, 0).toDate();
+		Date data =          new DateTime(2016, 12, 20, 5, 10, 0).toDate();
+		Date fimPeriodo =    new DateTime(2016, 12, 20, 5, 10, 0).toDate();
+		
+		//Act
+		boolean resultado1 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, true, true);
+		boolean resultado2 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, false, true);
+		boolean resultado3 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, true, false);
+		boolean resultado4 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, false, false);
+		
+		//Arrange
+		Assert.assertTrue(resultado1);
+		Assert.assertTrue(resultado2);
+		Assert.assertFalse(resultado3);
+		Assert.assertFalse(resultado4);
+	}
+	
+	@Test
+	public void dataDentro(){
+		Date inicioPeriodo = new DateTime(2016, 10, 20, 5, 10, 0).toDate();
+		Date data =          new DateTime(2016, 11, 20, 5, 10, 0).toDate();
+		Date fimPeriodo =    new DateTime(2016, 12, 20, 5, 10, 0).toDate();
+		
+		//Act
+		boolean resultado1 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, true, true);
+		boolean resultado2 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, false, true);
+		boolean resultado3 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, true, false);
+		boolean resultado4 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, false, false);
+		
+		//Arrange
+		Assert.assertTrue(resultado1);
+		Assert.assertTrue(resultado2);
+		Assert.assertTrue(resultado3);
+		Assert.assertTrue(resultado4);
+	}
+	
+	@Test
+	public void dataForaAntes(){
+		Date data =          new DateTime(2016, 9, 20, 5, 10, 0).toDate();
+		Date inicioPeriodo = new DateTime(2016, 10, 20, 5, 10, 0).toDate();
+		Date fimPeriodo =    new DateTime(2016, 12, 20, 5, 10, 0).toDate();
+		
+		//Act
+		boolean resultado1 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, true, true);
+		boolean resultado2 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, false, true);
+		boolean resultado3 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, true, false);
+		boolean resultado4 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, false, false);
+		
+		//Arrange
+		Assert.assertFalse(resultado1);
+		Assert.assertFalse(resultado2);
+		Assert.assertFalse(resultado3);
+		Assert.assertFalse(resultado4);
+	}
+	
+	@Test
+	public void dataForaDepois(){
+		Date inicioPeriodo = new DateTime(2016, 10, 20, 5, 10, 0).toDate();
+		Date fimPeriodo =    new DateTime(2016, 11, 20, 5, 10, 0).toDate();
+		Date data =          new DateTime(2016, 12, 20, 5, 10, 0).toDate();
+		
+		//Act
+		boolean resultado1 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, true, true);
+		boolean resultado2 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, false, true);
+		boolean resultado3 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, true, false);
+		boolean resultado4 = DataUtils.dataEstaDentroDoPeriodo(data, inicioPeriodo, fimPeriodo, false, false);
+		
+		//Arrange
+		Assert.assertFalse(resultado1);
+		Assert.assertFalse(resultado2);
+		Assert.assertFalse(resultado3);
+		Assert.assertFalse(resultado4);
+	}
+}
---
+++ src/test/java/DataUtilsColisaoPeriodoPeriodoTest.java
@@ -0,0 +1,76 @@
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Test;
+
+import ffnunes.utils.DataUtils;
+
+public class DataUtilsColisaoPeriodoPeriodoTest {
+	
+	@Test
+	public void periodosNaoColidemExclusive(){
+		Date inicioPeriodo1 = new DateTime(2016, 10, 10, 5, 10, 0).toDate();
+		Date fimPeriodo1 =    new DateTime(2016, 10, 20, 5, 10, 0).toDate();
+		
+		Date inicioPeriodo2 = new DateTime(2016, 10, 20, 5, 10, 0).toDate();
+		Date fimPeriodo2 =    new DateTime(2016, 10, 25, 5, 10, 0).toDate();
+		
+		//Act
+		boolean resultado1 = DataUtils.periodosColidem(inicioPeriodo1, fimPeriodo1, inicioPeriodo2, fimPeriodo2, false);
+		
+		
+		//Arrange
+		Assert.assertFalse(resultado1);
+	}
+	
+	@Test
+	public void periodosNaoColidemInclusive(){
+		Date inicioPeriodo1 = new DateTime(2016, 10, 10, 5, 10, 0).toDate();
+		Date fimPeriodo1 =    new DateTime(2016, 10, 15, 5, 10, 0).toDate();
+		
+		Date inicioPeriodo2 = new DateTime(2016, 10, 20, 5, 10, 0).toDate();
+		Date fimPeriodo2 =    new DateTime(2016, 10, 25, 5, 10, 0).toDate();
+		
+		//Act
+		boolean resultado1 = DataUtils.periodosColidem(inicioPeriodo1, fimPeriodo1, inicioPeriodo2, fimPeriodo2, true);
+		
+		
+		//Arrange
+		Assert.assertFalse(resultado1);
+	}
+	
+	@Test
+	public void periodosColidemExclusive(){
+		Date inicioPeriodo1 = new DateTime(2016, 10, 22, 5, 10, 0).toDate();
+		Date fimPeriodo1 =    new DateTime(2016, 10, 25, 5, 10, 0).toDate();
+		
+		Date inicioPeriodo2 = new DateTime(2016, 10, 24, 5, 10, 0).toDate();
+		Date fimPeriodo2 =    new DateTime(2016, 10, 26, 5, 10, 0).toDate();
+		
+		//Act
+		boolean resultado1 = DataUtils.periodosColidem(inicioPeriodo1, fimPeriodo1, inicioPeriodo2, fimPeriodo2, false);
+		
+		
+		//Arrange
+		Assert.assertTrue(resultado1);
+	}
+	
+	@Test
+	public void periodosColidemInclusive(){
+		Date inicioPeriodo1 = new DateTime(2016, 10, 22, 5, 10, 0).toDate();
+		Date fimPeriodo1 =    new DateTime(2016, 10, 24, 5, 10, 0).toDate();
+		
+		Date inicioPeriodo2 = new DateTime(2016, 10, 24, 5, 10, 0).toDate();
+		Date fimPeriodo2 =    new DateTime(2016, 10, 26, 5, 10, 0).toDate();
+		
+		//Act
+		boolean resultado1 = DataUtils.periodosColidem(inicioPeriodo1, fimPeriodo1, inicioPeriodo2, fimPeriodo2, true);
+		
+		
+		//Arrange
+		Assert.assertTrue(resultado1);
+	}
+	
+	
+}
